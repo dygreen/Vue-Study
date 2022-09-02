50cce6fa479bdf04d1e92966174f290812e616df
pdy <pdy@mtn.co.kr>
2022-09-02 17:49:28 +0900

    files add

--- src/App.vue
+++ src/App.vue
@@ -24,16 +24,16 @@
     </div>
   </nav>
 
-  <div class="container mt-4">
-    <h5>블로그입니다</h5>
-    <p>- Vue로 만듦 -</p>
-  </div>
+  <!--  router 링크 이동 -->
+  <router-link to="/">홈페이지</router-link>
+  <router-link to="/list">리스트페이지</router-link>
 
-  <List :블로그글="블로그글" />
+  <router-view :블로그글="블로그글"></router-view>
+
+  <!-- <List :블로그글="블로그글" /> -->
 </template>
 
 <script>
-import List from "./components/List.vue";
 import blog from './assets/blog.js';
 
 export default {
@@ -44,7 +44,7 @@ export default {
     }
   },
   components: {
-    List : List,
+
   },
 }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container mt-4">
+    <h5>블로그입니다</h5>
+    <p>- Vue로 만듦 -</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style>
+
+</style>
